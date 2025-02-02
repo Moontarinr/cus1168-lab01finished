d279a0d1d9ee35d98e588551cce284a517e0061f
Moontarinr <moontarin.rohman22@my.stjohns.edu>
2025-02-02 17:58:51 -0500

    finished lab

--- src/main/java/academy/javapro/StudentStreamLab.java
+++ src/main/java/academy/javapro/StudentStreamLab.java
@@ -16,10 +16,37 @@ public class StudentStreamLab {
 
         // TODO - Filtering: Students with GPA > 3.0
 
+        System.out.println("Students with GPA > 3.0:");
+        students.stream()
+                .filter(s -> s.getGpa() > 3.0)
+                .forEach(System.out::println);
+
         // TODO - Mapping: Extract names of students with GPA > 3.0
+ 
+        String highGpaNames = students.stream()
+                .filter(s -> s.getGpa() > 3.0)
+                .map(Student::getName)
+                .collect(Collectors.joining(", "));
+
+                System.out.println("\nHigh GPA student names: " + highGpaNames);
+
 
         // TODO - Reducing: Calculate the average GPA of all students
 
+        double averageGpa = students.stream()
+                .mapToDouble(Student::getGpa)
+                .average()
+                .orElse(0.0);
+
+        System.out.printf("\nAverage GPA: %.2f\n", averageGpa);
+        
         // TODO Collecting: Collect all "Junior" students into a list
+
+        List<Student> juniorStudents = students.stream()
+                .filter(s -> s.getCollegeYear().equals("Junior"))
+                .collect(Collectors.toList());
+
+        System.out.println("\nJuniors:");
+        juniorStudents.forEach(System.out::println);
     }
 }
